428ec62263776bfab450bf91bd5cafc6b09c6bf0
Andrés García <playsharp59@gmail.com>
2023-07-09 23:28:30 +0200

    Add types for auth routes

--- routes/auth.routes.ts
+++ routes/auth.routes.ts
@@ -1,31 +1,53 @@
-import { Router } from 'express'
+import { Request, Response, Router } from 'express'
 const router = Router()
 import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
 import isAuthenticated from '../middlewares/jwt.middleware'
 import User from './../models/User.model'
 import { Request as JWTRequest } from 'express-jwt'
+import { Error } from 'mongoose'
 const saltRounds = 10
 
-router.post('/register', (req, res) => {
-  const { password, username } = req.body
+interface RequestBody {
+  username: string
+  password: string
+}
 
+router.post('/register', (req: Request, res: Response): void => {
+  const { password, username }: RequestBody = req.body
   const salt = bcrypt.genSaltSync(saltRounds)
   const hashedPassword = bcrypt.hashSync(password, salt)
+
+  if (!password || !username) {
+    res
+      .status(400)
+      .json({ error: true, message: 'You must provide username and password' })
+
+    return
+  }
+
   User.create({ password: hashedPassword, username })
     .then(createdUser => {
       const { username, password, _id } = createdUser
       const user = { username, password, _id }
       res.status(201).json({ user })
     })
-    .catch(res => {
-      console.error(res)
+    .catch((error: Error) => {
+      console.error(error)
       res.status(500).json({ error: true, message: 'Internal server error.' })
     })
 })
 
-router.post('/login', (req, res, _) => {
-  const { username, password } = req.body
+router.post('/login', (req: Request, res: Response) => {
+  const { username, password }: RequestBody = req.body
+
+  if (!password || !username) {
+    res
+      .status(401)
+      .json({ error: true, message: 'You must provide username and password' })
+
+    return
+  }
 
   User.findOne({ username })
     .then(user => {
@@ -35,7 +57,10 @@ router.post('/login', (req, res, _) => {
       } else if (bcrypt.compareSync(password, user.password)) {
         const { username, _id } = user
         const payload = { username, _id }
-        const authToken = jwt.sign(payload, process.env.TOKEN_SECRET, { algorithm: 'HS256', expiresIn: '6h' })
+        const authToken = jwt.sign(payload, process.env.TOKEN_SECRET, {
+          algorithm: 'HS256',
+          expiresIn: '6h'
+        })
         res.status(200).json({ authToken })
       } else {
         res.status(401).json({ message: 'User cannot be authenticated.' })
@@ -47,8 +72,8 @@ router.post('/login', (req, res, _) => {
     })
 })
 
-router.get('/verify', isAuthenticated, (req: JWTRequest, res, _) => {
-  res.status(200).json(req.payload)
+router.get('/verify', isAuthenticated, (req: JWTRequest, res) => {
+  res.status(200).json(req.auth)
 })
 
 module.exports = router
