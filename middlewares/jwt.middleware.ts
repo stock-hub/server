4708446a8333168a59e78471082209ffcd2897d7
Andrés García <playsharp59@gmail.com>
2023-07-09 12:27:14 +0200

    Add middleware for authentication using JSON Web Tokens (JWT)

---
+++ middlewares/jwt.middleware.ts
@@ -0,0 +1,20 @@
+import { Request } from 'express'
+import { expressjwt as jwt } from 'express-jwt'
+
+const getTokenFromHeaders = (req: Request) => {
+  if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+    const token = req.headers.authorization.split(' ')[1]
+
+    return token
+  }
+  return null
+}
+
+const isAuthenticated = jwt({
+  secret: process.env.TOKEN_SECRET,
+  algorithms: ['HS256'],
+  requestProperty: 'payload',
+  getToken: getTokenFromHeaders
+})
+
+export default isAuthenticated
