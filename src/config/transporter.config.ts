c1ad4342c42baf04899d05b7729c7b22f45bd5e5
Andrés García <playsharp59@gmail.com>
2025-01-08 19:28:57 +0100

    fix: update transporter configuration

--- src/config/transporter.config.ts
+++ src/config/transporter.config.ts
@@ -3,12 +3,12 @@ import nodemailer from 'nodemailer'
 export const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: +process.env.EMAIL_PORT,
-  secure: false,
+  secure: !(process.env.NODE_ENV === 'development'),
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD
   },
   tls: {
-    rejectUnauthorized: false
+    rejectUnauthorized: !(process.env.NODE_ENV === 'development')
   }
 })
